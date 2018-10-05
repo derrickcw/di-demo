4708db712f9124961dc7c321d6268bfaa3afaa38
Derrick Chen <derrickchen0915@gmail.com>
2018-10-04 22:12:29 -0400

    Spring Component Scan

--- src/main/java/demo/springframework/didemo/DiDemoApplication.java
+++ src/main/java/demo/springframework/didemo/DiDemoApplication.java
@@ -7,8 +7,11 @@ import demo.springframework.didemo.controllers.SetterInjectedController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+//import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+// @ComponentScan(basePackages = {"demo.springframework.didemo"})
+// It will let spring auto searching the pages from "demo.springframework.didemo"
 public class DiDemoApplication {
 
     public static void main(String[] args) {
